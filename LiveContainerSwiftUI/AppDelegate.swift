6e62645a6b6f4ca128f14ebcba4d53a48d9bf6ba
Ruri <888mfvjygb@privaterelay.appleid.com>
2026-03-13 22:37:09 +0800

    Refactor fullscreen request handling for iPad
    Remove allowsSceneRotationNextRequests setting for iPad.

--- LiveContainerSwiftUI/AppDelegate.swift
+++ LiveContainerSwiftUI/AppDelegate.swift
@@ -71,7 +71,7 @@ class SceneDelegate: NSObject, UIWindowSceneDelegate, ObservableObject { // Make
         let tempRatio = UserDefaults.lcShared().double(forKey: "LCTempAspectRatio")
         if tempRatio > 0 && UIDevice.current.userInterfaceIdiom == .pad {
             newOptions._setRequestFullscreen(false)
-                newOptions.allowsSceneRotationNextRequests = true
+                
         } else {
             
             let isFull = UIScreen.main.bounds == (self.keyWindow?.bounds ?? .zero)

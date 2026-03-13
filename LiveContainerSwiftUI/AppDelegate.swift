e6d0d250bc166a51dad9196e2688c820c5eae224
Ruri <888mfvjygb@privaterelay.appleid.com>
2026-03-13 22:09:50 +0800

    Comment out fullscreen request for iPad
    Comment out the line that sets fullscreen request for iPad.

--- LiveContainerSwiftUI/AppDelegate.swift
+++ LiveContainerSwiftUI/AppDelegate.swift
@@ -70,7 +70,7 @@ class SceneDelegate: NSObject, UIWindowSceneDelegate, ObservableObject { // Make
         
         let tempRatio = UserDefaults.lcShared().double(forKey: "LCTempAspectRatio")
         if tempRatio > 0 && UIDevice.current.userInterfaceIdiom == .pad {
-            newOptions._setRequestFullscreen(false)
+            //newOptions._setRequestFullscreen(false)
         } else {
             
             let isFull = UIScreen.main.bounds == (self.keyWindow?.bounds ?? .zero)

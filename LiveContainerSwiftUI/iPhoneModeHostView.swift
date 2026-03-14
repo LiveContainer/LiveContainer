6ab8128a890a3bca940a9da54e8c8012dbfcffc4
Ruri <888mfvjygb@privaterelay.appleid.com>
2026-03-14 10:15:51 +0800

    Update iPhoneModeHostView.swift

--- LiveContainerSwiftUI/iPhoneModeHostView.swift
+++ LiveContainerSwiftUI/iPhoneModeHostView.swift
@@ -1,39 +1,31 @@
 import SwiftUI
 
+@available(iOS 16.1, *)
 struct IPhoneModeHostView: View {
-    let app: LCAppModel 
+    let app: LCAppModel
     @State var show = true
     @State var pid = 0
-    
-    
-    let fixedRatio: CGFloat = 9.0 / 16.0
+    let fixedRatio: CGFloat = 9.0 / 16.0 
 
     var body: some View {
         GeometryReader { geometry in
             ZStack {
-                
                 Color.black.ignoresSafeArea()
 
-                
                 AppSceneViewSwiftUI(
                     show: $show,
                     bundleId: app.appInfo.relativeBundlePath,
                     dataUUID: app.appInfo.dataUUID ?? "",
-                    
                     initSize: CGSize(width: geometry.size.height * fixedRatio, height: geometry.size.height),
                     onAppInitialize: { pid, error in
-                        if error == nil {
-                            self.pid = Int(pid)
-                        }
+                        if error == nil { self.pid = Int(pid) }
                     }
                 )
-                
                 .aspectRatio(fixedRatio, contentMode: .fit)
-                
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
             }
         }
         .ignoresSafeArea(.all, edges: .all)
-        .navigationBarHidden(true) 
+        .navigationBarHidden(true)
     }
 }

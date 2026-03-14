c94d54ac11c935609f2cac0773b1da9ed0ca03c3
Ruri <888mfvjygb@privaterelay.appleid.com>
2026-03-14 11:01:01 +0800

    Update iPhoneModeHostView.swift

--- LiveContainerSwiftUI/iPhoneModeHostView.swift
+++ LiveContainerSwiftUI/iPhoneModeHostView.swift
@@ -2,50 +2,47 @@ import SwiftUI
 
 @available(iOS 16.1, *)
 struct IPhoneModeHostView: View {
-    let app: LCAppModel
+    
+    let dataUUID: String
+    
     @State var show = true
     @State var pid = 0
-    
+    @EnvironmentObject var sharedModel: SharedModel 
+
     var body: some View {
-        GeometryReader { geometry in
-            ZStack {
-                
-                Color.black.ignoresSafeArea()
+        
+        if let app = (sharedModel.apps + sharedModel.hiddenApps).first(where: { $0.appInfo.dataUUID == dataUUID }) {
+            GeometryReader { geometry in
+                ZStack {
+                    Color.black.ignoresSafeArea()
 
-                
-                let targetSize = calculateIPhoneSize(in: geometry.size)
+                    let targetSize = calculateIPhoneSize(in: geometry.size)
 
-                AppSceneViewSwiftUI(
-                    show: $show,
-                    bundleId: app.appInfo.relativeBundlePath,
-                    dataUUID: app.appInfo.dataUUID ?? "",
-                    initSize: targetSize, 
-                    onAppInitialize: { pid, error in
-                        if error == nil { self.pid = Int(pid) }
-                    }
-                )
-               
-                .frame(width: targetSize.width, height: targetSize.height) 
-                .clipped() 
+                    AppSceneViewSwiftUI(
+                        show: $show,
+                        bundleId: app.appInfo.relativeBundlePath,
+                        dataUUID: app.appInfo.dataUUID ?? "",
+                        initSize: targetSize,
+                        onAppInitialize: { pid, error in
+                            if error == nil { self.pid = Int(pid) }
+                        }
+                    )
+                    .frame(width: targetSize.width, height: targetSize.height)
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
             }
-            
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .ignoresSafeArea()
+        } else {
+            Text("App Not Found")
         }
-        .ignoresSafeArea(.all)
-        .navigationBarHidden(true)
     }
 
-    
     func calculateIPhoneSize(in container: CGSize) -> CGSize {
         let ratio: CGFloat = 9.0 / 16.0
-        let availableHeight = container.height
-        let availableWidth = container.width
-        
-        var targetHeight = availableHeight
+        var targetHeight = container.height
         var targetWidth = targetHeight * ratio
-        
-        if targetWidth > availableWidth {
-            targetWidth = availableWidth
+        if targetWidth > container.width {
+            targetWidth = container.width
             targetHeight = targetWidth / ratio
         }
         return CGSize(width: targetWidth, height: targetHeight)

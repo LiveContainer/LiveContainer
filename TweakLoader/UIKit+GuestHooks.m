c33bb88fde4c0dc4703e90fa541eba894176c724
Ruri <888mfvjygb@privaterelay.appleid.com>
2026-03-16 15:51:39 +0800

    Update UIKit+GuestHooks.m

--- TweakLoader/UIKit+GuestHooks.m
+++ TweakLoader/UIKit+GuestHooks.m
@@ -13,7 +13,7 @@ __attribute__((constructor))
 static void UIKitGuestHooksInit() {
     if(!NSUserDefaults.lcGuestAppId) return;
     swizzle(UIDevice.class, @selector(userInterfaceIdiom), @selector(hook_userInterfaceIdiom));
-    swizzle(UIScreen.class, @selector(bounds), @selector(hook_UIScreen_bounds));
+    
     swizzle(UIWindow.class, @selector(setFrame:), @selector(hook_setFrame:));
     
     

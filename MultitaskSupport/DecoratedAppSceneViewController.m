d3d6c8ec51318978d4aece20d4a2856d7c6d5020
Ruri <888mfvjygb@privaterelay.appleid.com>
2026-03-22 16:10:47 +0800

    Update DecoratedAppSceneViewController.m

--- MultitaskSupport/DecoratedAppSceneViewController.m
+++ MultitaskSupport/DecoratedAppSceneViewController.m
@@ -405,12 +405,12 @@ void UIKitFixesInit(void) {
         CGFloat offsetX = (viewW - targetW) / 2.0;
         newFrame = CGRectMake(0, 0, targetW, viewH);
         dispatch_async(dispatch_get_main_queue(), ^{
-            vc.appSceneVC.contentView.frame = CGRectMake(offsetX, 0, targetW, viewH);
+            _appSceneVC.contentView.frame = CGRectMake(offsetX, 0, targetW, viewH);
         });
     } else {
         newFrame = CGRectMake(0, 0, viewW, viewH);
         dispatch_async(dispatch_get_main_queue(), ^{
-            vc.appSceneVC.contentView.frame = CGRectMake(0, 0, viewW, viewH);
+            _appSceneVC.contentView.frame = CGRectMake(0, 0, viewW, viewH);
         });
     }
     

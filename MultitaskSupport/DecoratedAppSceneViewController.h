bde36246c645bf0312127c3cd28e5d2835670aec
Ruri <888mfvjygb@privaterelay.appleid.com>
2026-03-25 14:30:59 +0800

    Update DecoratedAppSceneViewController.h

--- MultitaskSupport/DecoratedAppSceneViewController.h
+++ MultitaskSupport/DecoratedAppSceneViewController.h
@@ -13,6 +13,9 @@ API_AVAILABLE(ios(16.0))
 @property(nonatomic) BOOL isMaximized;
 @property(nonatomic) CGFloat scaleRatio;
 - (instancetype)initWindowName:(NSString*)windowName bundleId:(NSString*)bundleId dataUUID:(NSString*)dataUUID rootVC:(UIViewController*)rootVC;
+//⭐️⭐️⭐️
+- (void)maximizeWindow;
+//⭐️⭐️⭐️
 - (void)minimizeWindow;
 - (void)minimizeWindowPiP;
 - (void)unminimizeWindowPiP;

33e89cf8765dade7854f285e9a5a453fa8cf11b2
getBoolean <19920697+getBoolean@users.noreply.github.com>
2026-05-04 18:11:50 -0500

    Increase delay

--- LiveContainer/LCBootstrap.m
+++ LiveContainer/LCBootstrap.m
@@ -753,7 +753,7 @@ int LiveContainerMain(int argc, char *argv[]) {
                 if (!observer) return;
                 [[NSNotificationCenter defaultCenter] removeObserver:observer];
                 observer = nil;
-                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)),
+                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)),
                                dispatch_get_main_queue(), ^{
                     [[NSClassFromString(@"UIApplication") sharedApplication]
                         openURL:finalUrl options:@{} completionHandler:nil];

0736ec73ac957a4284c3a9db7262118df3ff8917
getBoolean <19920697+getBoolean@users.noreply.github.com>
2026-05-04 22:05:41 -0500

    add delay before opening virtual window deep link

--- MultitaskSupport/AppSceneViewController.m
+++ MultitaskSupport/AppSceneViewController.m
@@ -226,7 +226,8 @@
             notify_cancel(self.guestReadyToken);
             self.guestReadyToken = 0;
         }
-        dispatch_async(dispatch_get_main_queue(), ^{
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)),
+                       dispatch_get_main_queue(), ^{
             [self openURLScheme:url];
         });
     }
